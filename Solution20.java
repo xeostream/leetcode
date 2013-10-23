029d67828a489cc96e7e97b4ee18ac106da9c567
xeostream <iostreamin@gmail.com>
2013-10-23 14:44:43 +0800

    Surrounded Regions

--- Solution20.java
+++ Solution20.java
@@ -87,63 +87,65 @@ public class Solution20 {
 	}*/
 	// copy from https://github.com/mengli/leetcode/blob/master/Surrounded%20Regions.java, thanks for mengli
 	public class Point {
-            public int x;
-        public int y;
+		public int x;
+		public int y;
 
-        public Point(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
+		public Point(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
 
-    	public void solve(char[][] board) {
-        	int height = board.length;
-        	int width = 0;
-        	if (height > 0) {
-            		width = board[0].length;
-        	}
-                
-        	Queue<Point> q = new LinkedList<Point>();
-        	for (int i = 0; i < width; i++) {
-            		for (int j = 0; j < height; j++) {
-                		if ((i == 0 || i == width - 1 || j == 0 || j == height - 1) && board[i][j] == 'O') {
-                    			q.add(new Point(i, j));
-                		}
-            		}
-        	}
-                
-        	while (q.peek() != null) {
-           		Point p = q.remove();
-           		board[p.x][p.y] = '-';
-            		if (p.x > 0 && board[p.x - 1][p.y] == 'O') {
-                	q.add(new Point(p.x - 1, p.y));
-            		}
-            		if (p.x < width - 1 && board[p.x + 1][p.y] == 'O') {
-                		q.add(new Point(p.x + 1, p.y));
-            		}
-            		if (p.y > 0 && board[p.x][p.y - 1] == 'O') {
-                		q.add(new Point(p.x, p.y - 1));
-            		}
-            		if (p.y < height - 1 && board[p.x][p.y + 1] == 'O') {
-                		q.add(new Point(p.x, p.y + 1));
-            		}
-        	}
-                
-        	for (int i = 0; i < width; i++) {
-            		for (int j = 0; j < height; j++) {
-                		if (board[i][j] != 'X')
-                   			board[i][j] = board[i][j] == 'O' ? 'X' : 'O';
-            		}
-        	}
-    	}
-	
-	public static void main(String ...args) {
-		Solution20 s20  = new Solution20();
-		char[][] board = {"xxx".toCharArray(),"xox".toCharArray(),"xxx".toCharArray()};
+	public void solve(char[][] board) {
+		int height = board.length;
+		int width = 0;
+		if (height > 0) {
+			width = board[0].length;
+		}
+
+		Queue<Point> q = new LinkedList<Point>();
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				if ((i == 0 || i == width - 1 || j == 0 || j == height - 1)
+						&& board[i][j] == 'O') {
+					q.add(new Point(i, j));
+				}
+			}
+		}
+
+		while (q.peek() != null) {
+			Point p = q.remove();
+			board[p.x][p.y] = '-';
+			if (p.x > 0 && board[p.x - 1][p.y] == 'O') {
+				q.add(new Point(p.x - 1, p.y));
+			}
+			if (p.x < width - 1 && board[p.x + 1][p.y] == 'O') {
+				q.add(new Point(p.x + 1, p.y));
+			}
+			if (p.y > 0 && board[p.x][p.y - 1] == 'O') {
+				q.add(new Point(p.x, p.y - 1));
+			}
+			if (p.y < height - 1 && board[p.x][p.y + 1] == 'O') {
+				q.add(new Point(p.x, p.y + 1));
+			}
+		}
+
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				if (board[i][j] != 'X')
+					board[i][j] = board[i][j] == 'O' ? 'X' : 'O';
+			}
+		}
+	}
+
+	public static void main(String... args) {
+		Solution20 s20 = new Solution20();
+		char[][] board = { "xxx".toCharArray(), "xox".toCharArray(),
+				"xxx".toCharArray() };
 		s20.solve(board);
 		for (char[] b : board) {
 			for (char bo : b) {
-				System.out.print(bo+ " ");
+				System.out.print(bo + " ");
 			}
 			System.out.println();
 		}
