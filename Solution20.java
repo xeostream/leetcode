aa8b2724dd5d78d280374da8c7d847cc2901ef3e
xeostream <iostreamin@gmail.com>
2013-10-23 14:39:37 +0800

    Surrounded Regions format

--- Solution20.java
+++ Solution20.java
@@ -96,46 +96,46 @@ public class Solution20 {
         }
     }
 
-    public void solve(char[][] board) {
-        int height = board.length;
-        int width = 0;
-        if (height > 0) {
-            width = board[0].length;
-        }
+    	public void solve(char[][] board) {
+        	int height = board.length;
+        	int width = 0;
+        	if (height > 0) {
+            		width = board[0].length;
+        	}
                 
-        Queue<Point> q = new LinkedList<Point>();
-        for (int i = 0; i < width; i++) {
-            for (int j = 0; j < height; j++) {
-                if ((i == 0 || i == width - 1 || j == 0 || j == height - 1) && board[i][j] == 'O') {
-                    q.add(new Point(i, j));
-                }
-            }
-        }
+        	Queue<Point> q = new LinkedList<Point>();
+        	for (int i = 0; i < width; i++) {
+            		for (int j = 0; j < height; j++) {
+                		if ((i == 0 || i == width - 1 || j == 0 || j == height - 1) && board[i][j] == 'O') {
+                    			q.add(new Point(i, j));
+                		}
+            		}
+        	}
                 
-        while (q.peek() != null) {
-            Point p = q.remove();
-            board[p.x][p.y] = '-';
-            if (p.x > 0 && board[p.x - 1][p.y] == 'O') {
-                q.add(new Point(p.x - 1, p.y));
-            }
-            if (p.x < width - 1 && board[p.x + 1][p.y] == 'O') {
-                q.add(new Point(p.x + 1, p.y));
-            }
-            if (p.y > 0 && board[p.x][p.y - 1] == 'O') {
-                q.add(new Point(p.x, p.y - 1));
-            }
-            if (p.y < height - 1 && board[p.x][p.y + 1] == 'O') {
-                q.add(new Point(p.x, p.y + 1));
-            }
-        }
+        	while (q.peek() != null) {
+           		Point p = q.remove();
+           		board[p.x][p.y] = '-';
+            		if (p.x > 0 && board[p.x - 1][p.y] == 'O') {
+                	q.add(new Point(p.x - 1, p.y));
+            		}
+            		if (p.x < width - 1 && board[p.x + 1][p.y] == 'O') {
+                		q.add(new Point(p.x + 1, p.y));
+            		}
+            		if (p.y > 0 && board[p.x][p.y - 1] == 'O') {
+                		q.add(new Point(p.x, p.y - 1));
+            		}
+            		if (p.y < height - 1 && board[p.x][p.y + 1] == 'O') {
+                		q.add(new Point(p.x, p.y + 1));
+            		}
+        	}
                 
-        for (int i = 0; i < width; i++) {
-            for (int j = 0; j < height; j++) {
-                if (board[i][j] != 'X')
-                    board[i][j] = board[i][j] == 'O' ? 'X' : 'O';
-            }
-        }
-    }
+        	for (int i = 0; i < width; i++) {
+            		for (int j = 0; j < height; j++) {
+                		if (board[i][j] != 'X')
+                   			board[i][j] = board[i][j] == 'O' ? 'X' : 'O';
+            		}
+        	}
+    	}
 	
 	public static void main(String ...args) {
 		Solution20 s20  = new Solution20();
