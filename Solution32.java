954245ee728b092d0c7db428b9d411f96e069c69
xeostream <iostreamin@gmail.com>
2013-10-29 12:38:25 +0800

    Candy format

--- Solution32.java
+++ Solution32.java
@@ -9,20 +9,24 @@ public class Solution32 {
 		if (ratings == null || ratings.length == 0)
 			return 0;
 		int len = ratings.length;
+		int sum = 0;
 		int[] cnt = new int[len];
-		cnt[0] = 1;
-		for (int i = 1; i < len; i++) {
+		for (int i = 0; i < len; i++)
+			cnt[i] = 1;
+		for (int i = 1; i < len; i++)
 			if (ratings[i] > ratings[i-1])
-				cnt[i] = cnt[i-1] - 1;
-			else if (ratings[i] < ratings[i-1])
 				cnt[i] = cnt[i-1] + 1;
-		}
-		return cnt[len-1];
+		for (int i = len - 1; i > 0; i--)
+			if (ratings[i] < ratings[i-1] && cnt[i-1] <= cnt[i])
+				cnt[i-1] = cnt[i] + 1;
+		for (int i : cnt)
+			sum += i;
+		return sum;
 	}
 	
 	public static void main(String ...args) {
 		Solution32 s32 = new Solution32();
-		int[] ratings = {1,2};
+		int[] ratings = {1,2,2,1};
 		System.out.println(s32.candy(ratings));
 	}
 }
