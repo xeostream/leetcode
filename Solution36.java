89fce5b56eb690c10490f9dec9f5090b4b39295d
xeostream <iostreamin@gmail.com>
2013-10-30 21:29:28 +0800

    Sum Root to Leaf Numbers

---
+++ Solution36.java
@@ -0,0 +1,46 @@
+/*
+ * Sum Root to Leaf Numbers
+ */
+
+/**
+ *
+ * @author Arthur
+ */
+public class Solution36 {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) {val = x;}
+    }
+    
+    public int sumNumbers(TreeNode root) {
+        int sum = 0;
+        if (root != null) {
+            String s = "" + root.val;
+            sum = strToNumber(s, root.left) + strToNumber(s, root.right);
+        }
+        return sum;
+    }
+    
+    public int strToNumber(String s, TreeNode node) {
+        if (node == null) return 0;
+        if (node.left == null && node.right == null)
+            return Integer.parseInt(s+node.val);
+        else if (node.left == null)
+            return strToNumber(s+node.val, node.right);
+        else if (node.right == null)
+            return strToNumber(s+node.val, node.left);
+        else
+            return strToNumber(s+node.val, node.left) + strToNumber(s+node.val, node.right);
+    }
+    
+    public static void main(String ...args) {
+        Solution36 s36 = new Solution36();
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.right = new TreeNode(4);
+        System.out.println(s36.sumNumbers(root));
+    }
+}
