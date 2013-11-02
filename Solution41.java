7bb3d268d3ff9378cea813806c94e569f430d51e
xeostream <iostreamin@gmail.com>
2013-11-02 20:50:56 +0800

    Binary Tree Maximum Path Sum

---
+++ Solution41.java
@@ -0,0 +1,19 @@
+/*
+ * Binary Tree Maximum Path Sum
+ * Given a binary tree, find the maximum path sum
+ * The path may start and end at any node in the tree
+ */
+public class Solution41 {
+	class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+		TreeNode(int x) {val = x;}
+	}
+	
+	public int maxPathSum(TreeNode root) {
+		int sum = 0;
+		
+		return sum;
+	}
+}
