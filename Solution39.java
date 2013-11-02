83d48603b96f81777abe1bbee062cfe41e13a614
xeostream <iostreamin@gmail.com>
2013-11-02 22:22:11 +0800

    Word Ladder II

--- Solution39.java
+++ Solution39.java
@@ -18,9 +18,8 @@ import java.util.HashSet;
  * ]
  */
 public class Solution39 {
-	private ArrayList<ArrayList<String>> result = new ArrayList<ArrayList<String>>();
-	private HashSet<String> record = new HashSet<String>();
 	
+	/*
 	public ArrayList<ArrayList<String>> findLadders(String start, String end,
 			HashSet<String> dict) {
 		record.add(start);
@@ -74,6 +73,45 @@ public class Solution39 {
 				temp[i] = j;
 			}
 		}
+	}*/
+	//time exceed
+	public ArrayList<ArrayList<String>> findLadders(String start, String end, HashSet<String> dict){
+	    ArrayList<ArrayList<String>> ret = new ArrayList<ArrayList<String>>();
+	    ArrayList<String> path = new ArrayList<String>();
+	    wordLadderIIHelper(start, end, dict, path, ret);
+
+	    int min = 0;
+	    for(ArrayList<String> item : ret){
+	        min = min == 0 || item.size() < min ? min = item.size() : min;
+	    }
+
+	    for(int i = 0; i < ret.size(); i++){
+	        if(ret.get(i).size() > min)
+	            ret.remove(i--); // REMEMBER the  "i--" when removing elements from ArrayList
+	    }
+	    return ret;
+	}
+	public void wordLadderIIHelper(String start, String end, HashSet<String> dict, ArrayList<String> path, ArrayList<ArrayList<String>> ret){
+	    path.add(start);
+	    char[] sArr = start.toCharArray();
+	    for(int i = 0; i < sArr.length; i++){
+	        char origin = sArr[i];
+	        for(char c = 'a'; c <= 'z'; c++){
+	            if(c != origin){
+	                sArr[i] = c;
+	                String temp = new String(sArr);
+	                if(temp.equals(end)){
+	                    path.add(temp);
+	                    ret.add(new ArrayList<String>(path));
+	                    path.remove(path.size() - 1);
+	                }
+	                if(dict.contains(temp) && !path.contains(temp)) 
+	                    wordLadderIIHelper(temp, end, dict, path, ret);
+	            }
+	        }
+	        sArr[i] = origin;
+	    }
+	    path.remove(path.size() - 1);
 	}
 	
 	public static void main(String ...args) {
