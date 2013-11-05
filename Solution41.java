73246831913aef643b43c39e1718dfc45cc03508
xeostream <iostreamin@gmail.com>
2013-11-05 22:55:53 +0800

    /*

--- Solution41.java
+++ Solution41.java
@@ -4,7 +4,7 @@
  * The path may start and end at any node in the tree
  */
 public class Solution41 {
-	class TreeNode {
+	static class TreeNode {
 		int val;
 		TreeNode left;
 		TreeNode right;
@@ -12,8 +12,29 @@ public class Solution41 {
 	}
 	
 	public int maxPathSum(TreeNode root) {
-		int sum = 0;
-		
-		return sum;
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        int max[] = new int[1]; // this is used to keep max val
+        max[0] = Integer.MIN_VALUE;
+        localSum(root, max);
+        return max[0];       
+    }
+     
+    public int localSum(TreeNode root, int[] max) {
+        if (root == null) return 0;
+        int lsum = localSum(root.left, max);
+        int rsum = localSum(root.right, max);
+        int csum = Math.max(root.val, Math.max(root.val + lsum, root.val + rsum));
+         
+        max[0] = Math.max(max[0], Math.max(csum, lsum + root.val + rsum));
+        return csum;
+    }
+	
+	public static void main(String ...args) {
+		Solution41 s41 = new Solution41();
+		TreeNode root = new TreeNode(1);
+		root.left = new TreeNode(2);
+		root.right = new TreeNode(3);
+		System.out.println(s41.maxPathSum(root));
 	}
 }
