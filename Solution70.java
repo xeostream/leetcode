926de9c00ac2f4e6e7c4b37b95a90577715f3bfb
xeostream <iostreamin@gmail.com>
2013-12-06 12:57:27 +0800

    Recover Binary Search Tree

---
+++ Solution70.java
@@ -0,0 +1,15 @@
+/*
+ * Recover Binary Search Tree
+ * Two elements of a binary search tree (BST) are swapped by mistake.
+ * Recover the tree without changing its structure.
+ */
+public class Solution70 {
+	static class TreeNode {
+		int val;
+		TreeNode left, right;
+		TreeNode(int x) { val = x; }
+	}
+	public void recoverTree(TreeNode root) {
+		
+	}
+}
