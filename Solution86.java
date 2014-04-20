3af770698db69438ac576977eacba08ded0dc45d
xeostream <iostreamin@gmail.com>
2014-04-20 21:34:36 +0800

    Remove Duplicates from Sorted Array II

--- Solution86.java
+++ Solution86.java
@@ -2,15 +2,14 @@ public class Solution86 {
     public int removeDuplicates(int[] A) {
         if (null == A) return 0;
         if (A.length < 2) return A.length;
-        int cnt = 0;
-        int[] B = new int[A.length + 2];
-        B[A.length] = Integer.MAX_VALUE, B[A.length + 1] = Integer.MAX_VALUE;
-        System.arraycopy(A, 0, B, 0, A.length);
-        boolean key = false;
-        for (int i = 0; i < A.length; i++) {
-            if (B[i] != B[i + 2])
-                cnt++;
+        int num = 1, temp = A[1];
+        for (int i =2; i < A.length; i++) {
+            if (A[i] != A[i-2]) {
+                A[num++] = temp;
+                temp = A[i];
+            }
         }
+        A[num++] = temp;
         /*Map<Integer, Integer> map = new HashMap<Integer, Integer>();
         for (int i : A) {
             if (!map.containsKey(i)) {
@@ -22,7 +21,7 @@ public class Solution86 {
             }
         }*/
 
-        return cnt;
+        return num;
     }
     public static void main(String ...args) {
         Solution86 s = new Solution86();
